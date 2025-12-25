41d36609e8769b15dbc6b0f6fac513665a98f762
Abdulrahman Rafat Ahmed <140813224+Abdul-Rahman-Rafat@users.noreply.github.com>
2025-12-25 07:01:57 +0200

    register done with responsive

--- script.js
+++ script.js
@@ -9,5 +9,25 @@ burgerMenu.onclick=()=>{
 ///////////////////////////////////////////////////////////////////
 
 
+// home-arrow
+let arr = document.querySelector(".up-home-arrow");
+window.addEventListener("scroll", () => {
+    if(scrollY >250){
+        arr.style.display="block";
+    }
+    else{
+        arr.style.display="none";
+    }
+})
+
+function return_home(){
+    window.scrollTo({
+        top : 0,
+        behavior : "smooth"}
+    );
+}
+// end of home-arrow 
+
+
 
 
